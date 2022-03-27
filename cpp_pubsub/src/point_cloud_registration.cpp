c325a8ecd8ffda38f1e757fbe982114052c0fe03
Ata Parlar <ataparlars5@gmail.com>
2022-03-27 15:28:32 +0300

    added ross:ok() and published right things in right place

--- cpp_pubsub/src/point_cloud_registration.cpp
+++ cpp_pubsub/src/point_cloud_registration.cpp
@@ -6,6 +6,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <geometry_msgs/msg/quaternion_stamped.hpp>
@@ -91,6 +92,8 @@ public:
     std::string pcd_file2;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_2;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc_publisher_aligned;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pc_publisher_error;
 
     using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
     using PointSource = pcl::PointXYZ;
@@ -116,6 +119,10 @@ public:
 
         return cloud;
     };
+
+
+
+    
 
 
 
@@ -148,25 +155,21 @@ public:
 
         pc_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/points1", 10);
         pc_publisher_2 = this->create_publisher<sensor_msgs::msg::PointCloud2>("/points2", 10);
+        pc_publisher_aligned = this->create_publisher<sensor_msgs::msg::PointCloud2>("/points_aligned", 10);
+        pc_publisher_error = this->create_publisher<std_msgs::msg::Float64>("/points_error", 1);
 
         // publish on ros2
 
         sensor_msgs::msg::PointCloud2 ros_cloud1;
         sensor_msgs::msg::PointCloud2 ros_cloud2;
+        sensor_msgs::msg::PointCloud2 aligned_ros;
+        std_msgs::msg::Float64 score;
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud1 = file_loader("pcd_file1");
         pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud2 = file_loader("pcd_file2");
 
         pcl::toROSMsg(*pcl_cloud1, ros_cloud1);
         pcl::toROSMsg(*pcl_cloud2, ros_cloud2);
-
-
-        
-        ros_cloud1.header.frame_id = "map";
-        ros_cloud1.header.stamp = this->get_clock()->now();
-
-        ros_cloud2.header.frame_id = "map";
-        ros_cloud2.header.stamp = this->get_clock()->now();
 
         RCLCPP_INFO_STREAM(this->get_logger(), "Loaded "
                                                    << pcl_cloud1->width * pcl_cloud1->height
@@ -177,6 +180,8 @@ public:
 
         pc_publisher_->publish(ros_cloud1);
         pc_publisher_2->publish(ros_cloud2);
+
+
 
         // gicp part
 
@@ -200,23 +205,44 @@ public:
         gicp.align(*aligned_cloud);
 
         Eigen::Matrix4f src2tgt   = gicp.getFinalTransformation();
-        double score              = gicp.getFitnessScore();
+        score.data                = gicp.getFitnessScore();
         bool is_converged         = gicp.hasConverged();
 
         RCLCPP_INFO_STREAM(this->get_logger(), "transform matrix: " << "\n" << src2tgt << "\n"
-                                            << "fitness score: " << score << "\n"
+                                            << "fitness score: " << score.data << "\n"
                                             << "is converged: " << is_converged << "\n");
 
 
-        // int x = 0;
-        // while(true){
-        //   if(x % 8000000 == 0) {
-        //     publisher_->publish(ros_cloud1);
-        //     publisher_2->publish(ros_cloud2);
-        //     //RCLCPP_INFO(this->get_logger(), "published %d", x);
-        //     }
-        //   x++;
-        // }
+
+        // colorize part
+
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl1_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl2_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr align_colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+        colorize(*pcl_cloud1, *pcl1_colored, {255, 0, 0});
+        colorize(*pcl_cloud2, *pcl2_colored, {0, 255, 0});
+        colorize(*aligned_cloud, *align_colored, {0, 0, 255});
+
+        pcl::toROSMsg(*aligned_cloud, aligned_ros);
+        
+
+        while (rclcpp::ok())
+        {
+            ros_cloud1.header.frame_id = "map";
+            ros_cloud1.header.stamp = this->get_clock()->now();
+
+            ros_cloud2.header.frame_id = "map";
+            ros_cloud2.header.stamp = this->get_clock()->now();
+
+            aligned_ros.header.frame_id = "map";
+            aligned_ros.header.stamp = this->get_clock()->now();
+
+            pc_publisher_->publish(ros_cloud1);
+            pc_publisher_2->publish(ros_cloud2);
+            pc_publisher_aligned->publish(aligned_ros);
+            pc_publisher_error->publish(score);
+        }
+
     };
 };
 
